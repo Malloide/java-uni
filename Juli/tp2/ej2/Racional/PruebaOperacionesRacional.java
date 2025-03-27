22cc671002ee8bf75f85a444ddac3ef34d312fe4
Malloide <mallojulian'04@gmail.com>
2025-03-27 15:01:11 -0300

    racional completo

---
+++ Juli/tp2/ej2/Racional/PruebaOperacionesRacional.java
@@ -0,0 +1,25 @@
+package Racional;
+
+// Removed incorrect import statement
+
+class PruebaOperacionesRacional {
+
+	public static void main(String[] args) {
+
+		// https://www.superprof.es/apuntes/escolar/matematicas/aritmetica/racionales/numeros-racionales-2.html#tema_suma-y-resta-de-numeros-racionales
+					
+		Racional r1 = new Racional(5, 4);
+		System.out.println("Value of r1: " + r1);
+		Racional r2 = new Racional(1, 6);
+		System.out.println("Value of r2: " + r2);
+		
+		// Realizar operaciones con racionales
+		// 5/4 + 1/6 = 17/12
+		// 5/4 - 1/6 = 13/12
+		// 5/4 * 1/6 = 5/24
+		// 5/4 / 1/6 = 15/2
+		// (5/4)^2 = 25/16
+						
+	}
+
+}
